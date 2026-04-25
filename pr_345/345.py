3a5e4791d313e341bece01a3954d23446ab57e3a
Lei200505 <leventesimonics@gmail.com>
2026-04-25 16:34:44 +0200

    project 345

---
+++ pr_345/345.py
@@ -0,0 +1,36 @@
+import pulp as pl
+
+def max_matrix_sum(matrix):
+    #-------------Inicializálás-------------
+    rows = len(matrix)
+    cols = len(matrix[0])
+    # Változók
+    x = pl.LpVariable.dicts("x", (range(rows), range(cols)), cat="Binary")
+
+    model = pl.LpProblem("Max_Matrix_Sum", pl.LpMaximize)
+
+    #---------------Feltételek---------------
+    # Minden sorban és oszlopban legfeljebb egy elem lehet
+    for i in range(rows):
+        model += sum(x[i][j] for j in range(cols)) <= 1
+    for j in range(cols):
+        model += sum(x[i][j] for i in range(rows)) <= 1
+
+    # Célfüggvény
+    model += sum(matrix[i][j] * x[i][j] for i in range(rows) for j in range(cols))
+
+    
+    model.solve(pl.PULP_CBC_CMD(msg=0))
+    max_sum = pl.value(model.objective)
+    return max_sum
+
+
+with open("pr_euler_345.txt") as f:
+    lines = f.read().strip().split("\n")
+    matrix = []
+    for line in lines:
+        row = list(map(int, line.split()))
+        matrix.append(row)
+
+result = max_matrix_sum(matrix)
+print(result)
